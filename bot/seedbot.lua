8006567f64b6908773e232b3168f80e68d712e81
amirkhafan <amirhooo864@gmail.com>
2016-01-21 17:56:01 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {amirhossein_0079},--Sudo users
+    sudo_users = {103373495},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
